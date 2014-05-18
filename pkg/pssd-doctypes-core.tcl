b7bafed45be871312985f3eaf017d31d166c89bf
Justin Chang <justin.chang@sydney.edu.au>
2014-05-19 09:55:14 +1000

    edited bmri:pssd.identifeirs docType

--- pkg/pssd-doctypes-core.tcl
+++ pkg/pssd-doctypes-core.tcl
@@ -14,9 +14,6 @@ proc createDocType_pssd_subject_identifiers { ns } {
         	:element -name "subject-bmri-id" -type "string" -index true -min-occurs 1 -max-occurs 1 < \
             	:description "The BMRI ID for a subject, used to link a subject across multiple projects" \
             > \
-    		:element -name "subject-project-id" -type "string" -index true -min-occurs 1 -max-occurs 1 < \
-            	:description "The project specific ID for a subject" \
-            > \
             :element -name "other-identifiers" -type "string" -index true -min-occurs 0 -max-occurs infinity < \
             	:description "other identifiers for the subject" \
             > \
