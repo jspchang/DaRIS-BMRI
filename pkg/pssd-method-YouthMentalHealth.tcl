d52bef652a62d7c6f92c584d9165236b39dfca8b
jspchang <justin.chang@sydney.edu.au>
2015-02-03 16:55:46 +1100

    remove a line in the script which is used for administration activity to be undertaken only if you make some kind of change that requires you to (carefully) push a change to a Method down the PSSD tree.  It something that should always be done carefully and knowingly.

--- pkg/pssd-method-YouthMentalHealth.tcl
+++ pkg/pssd-method-YouthMentalHealth.tcl
@@ -491,13 +491,6 @@ set id2 [xvalue id [om.pssd.method.for.subject.update $args] ]
 		set methId $id2
 	}
 	
-# Fix up any extant ex-method
-
-set exMethods [xvalues cid [asset.query :where xpath(pssd-object/type)='ex-method' and xpath(pssd-ex-method/method/id)='$methId' :action get-cid]]
-
-foreach exMethod $exMethods {
-	om.pssd.ex-method.method.replace :id $exMethod :method $methId :recursive false
-}
 
 return $methId
 
